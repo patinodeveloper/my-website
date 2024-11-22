bf960fb2a3d43f0a9c6e4e3bf260683885a1fec4
patinodeveloper <pepe_toni_02@hotmail.com>
2024-11-21 22:37:44 -0600

    refactor(component): extract 'projects' section into its own component with scoped styles

--- src/components/BodyView.jsx
+++ src/components/BodyView.jsx
@@ -1,5 +1,6 @@
 import api from '../assets/api.png'
 import { AboutMeSection } from './AboutMeSection';
+import { ProjectsSection } from './ProjectsSection';
 
 export const BodyView = () => {
 
@@ -8,53 +9,7 @@ export const BodyView = () => {
             {/* About Me Section */}
             <AboutMeSection />
             {/* <!-- Projects Section --> */}
-            <section id='projects' className="projects">
-                <h2 className="section__title">Proyectos</h2>
-                <div className="projects__container">
-                    <div className="project__item">
-                        <h3 className="project__title">Taller de Reparación de Dispositivos Electrónicos</h3>
-                        <p className="project__description">
-                            Software integral para gestionar inventario, reparaciones y pagos en un
-                            taller de reparación.
-                        </p>
-                        <div className="project__icons">
-                            <i className="devicon-java-plain colored"></i>
-                            <i className="devicon-mysql-original colored"></i>
-                        </div>
-                        <a href='https://github.com/patinodeveloper/Gestion-Reparacion-de-Dispositivos'
-                            target='_blank' className='btn-project'>Ver Proyecto</a>
-                    </div>
-                    <div className="project__item">
-                        <h3 className="project__title">Sistema de Citas Médicas</h3>
-                        <p className="project__description">
-                            API backend para la gestión de citas médicas, incluyendo horarios de doctores,
-                            especialidades y pacientes.
-                        </p>
-                        <div className="project__icons">
-                            <i className="devicon-java-plain colored"></i>
-                            <i className="devicon-spring-original colored"></i>
-                            <i className="devicon-mysql-original colored"></i>
-                        </div>
-                        <a href='https://github.com/patinodeveloper/MedMeet---Medical-Appointments-API'
-                            target="_blank" className='btn-project'>Ver Proyecto</a>
-                    </div>
-                    <div className="project__item">
-                        <h3 className="project__title">Sistema Web ERP Veterinaria</h3>
-                        <p className="project__description">
-                            Sistema web para la gestión de clientes, mascotas y servicios en una clínica veterinaria.
-                        </p>
-                        <div className="project__icons">
-                            <i className="devicon-mysql-original colored"></i>
-                            <i className="devicon-javascript-plain colored"></i>
-                            <i className="devicon-html5-plain-wordmark colored"></i>
-                            <i className="devicon-bootstrap-plain colored"></i>
-                            <i className="devicon-php-plain colored"></i>
-                        </div>
-                        <a href='https://github.com/patinodeveloper/Veterinaria-MVC-Pre'
-                            target='_blank' className='btn-project'>Ver Proyecto</a>
-                    </div>
-                </div>
-            </section>
+            <ProjectsSection />
 
             {/* <!-- Technologies Section --> */}
             <section id='technologies' className="technologies">
