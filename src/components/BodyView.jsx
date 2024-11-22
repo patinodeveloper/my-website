b31e7babee23f122a4ed535d41a200a7e575648f
patinodeveloper <pepe_toni_02@hotmail.com>
2024-11-21 22:51:58 -0600

    refactor(component): extract 'technologies' section into its own component with scoped styles

--- src/components/BodyView.jsx
+++ src/components/BodyView.jsx
@@ -1,6 +1,6 @@
-import api from '../assets/api.png'
 import { AboutMeSection } from './AboutMeSection';
 import { ProjectsSection } from './ProjectsSection';
+import { TechnologiesSection } from './TechnologiesSection';
 
 export const BodyView = () => {
 
@@ -8,63 +8,12 @@ export const BodyView = () => {
         <>
             {/* About Me Section */}
             <AboutMeSection />
+
             {/* <!-- Projects Section --> */}
             <ProjectsSection />
 
             {/* <!-- Technologies Section --> */}
-            <section id='technologies' className="technologies">
-                <h2 className="section__title">Tecnologías</h2>
-                <div className="technologies__container">
-                    <div className="technology__item">
-                        <i className="devicon-java-plain colored"></i>
-                        <p className="technology__name">Java</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-spring-original colored"></i>
-                        <p className="technology__name">Spring Boot</p>
-                    </div>
-                    <div className="technology__item">
-                        <img src={api} alt="API REST" />
-                        <p className="technology__name">API REST</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-javascript-plain colored"></i>
-                        <p className="technology__name">JavaScript</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-react-original colored"></i>
-                        <p className="technology__name">ReactJS</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-mysql-original colored"></i>
-                        <p className="technology__name">MySQL</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-mongodb-plain colored"></i>
-                        <p className="technology__name">MongoDB</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-html5-plain-wordmark colored"></i>
-                        <p className="technology__name">HTML5</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-css3-plain-wordmark colored"></i>
-                        <p className="technology__name">CSS3</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-reactbootstrap-original colored"></i>
-                        <p className="technology__name">Bootstrap 5</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-git-plain colored"></i>
-                        <p className="technology__name">Git</p>
-                    </div>
-                    <div className="technology__item">
-                        <i className="devicon-github-original"></i>
-                        <p className="technology__name">Github</p>
-                    </div>
-                </div>
-            </section>
+            <TechnologiesSection />
         </>
 
     );
